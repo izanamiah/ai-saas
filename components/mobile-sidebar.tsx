2f5f9ffcdd43c1e234260ef22885a300d82fc16b
izanamiah <jeremiahliu@live.com>
2023-07-20 18:43:56 -0400

    added sheet sidebar, fixed hydration error

---
+++ components/mobile-sidebar.tsx
@@ -0,0 +1,32 @@
+import { Menu } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import Sidebar from "@/components/sidebar";
+import { useEffect, useState } from "react";
+
+const MobileSidebar = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  //this fix the hydration error
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Button variant="ghost" size="icon" className="md:hidden">
+          <Menu />
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left" className="p-0">
+        <Sidebar />
+      </SheetContent>
+    </Sheet>
+  );
+};
+export default MobileSidebar;
