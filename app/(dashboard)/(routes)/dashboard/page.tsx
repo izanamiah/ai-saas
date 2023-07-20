bdbe048938a5deb2d1c19fff180b1dcccecc4405
izanamiah <jeremiahliu@live.com>
2023-07-20 19:01:38 -0400

    added constent file

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,8 +1,19 @@
 "use client";
+
+import { tools } from "@/constants";
+
 const DashboardPage = () => {
   return (
     <>
-      <p>Dashboard Page</p>
+      <div className="mb-8 space-y-4">
+        <h2 className="text-2xl md:text-4xl font-bold text-center">
+          Explore the power of AI
+        </h2>
+        <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
+          Chat with the smartest AI - Experience the power of AI
+        </p>
+      </div>
+      <div className="px-4 md:px-20 lg:px-32 space-y-4"></div>
     </>
   );
 };
