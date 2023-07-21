f929a2b274624dc052352132b1e3df1d3f83de39
izanamiah <jeremiahliu@live.com>
2023-07-20 20:39:42 -0400

    added dashboard card

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,8 +1,13 @@
 "use client";
 
+import { Card } from "@/components/ui/card";
 import { tools } from "@/constants";
+import { cn } from "@/lib/utils";
+import { ArrowRight } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 const DashboardPage = () => {
+  const router = useRouter();
   return (
     <>
       <div className="mb-8 space-y-4">
@@ -13,7 +18,25 @@ const DashboardPage = () => {
           Chat with the smartest AI - Experience the power of AI
         </p>
       </div>
-      <div className="px-4 md:px-20 lg:px-32 space-y-4"></div>
+      <div className="px-4 md:px-20 lg:px-32 space-y-4">
+        {tools.map((tool) => (
+          <Card
+            onClick={() => router.push(tool.href)}
+            key={tool.href}
+            className=" p-4 border-black/5 flex items-center
+            justify-between hover:shadow-md transition cursor-pointer"
+          >
+            <div className="flex items-center gap-x-4">
+              <div className={cn("p-2 w-fit rounded-md", tool.bgColor)}>
+                <tool.icon className={cn("w-8 h-8", tool.color)} />
+              </div>
+              <div className="font-semibold">{tool.label}</div>
+            </div>
+
+            <ArrowRight className="w-5 h-5" />
+          </Card>
+        ))}
+      </div>
     </>
   );
 };
