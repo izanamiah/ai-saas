9513446312bed8d654ce12672ffa69c1eabd2eea
izanamiah <jeremiahliu@live.com>
2023-07-21 21:48:40 -0400

    added error toast

--- app/(dashboard)/(routes)/code/page.tsx
+++ app/(dashboard)/(routes)/code/page.tsx
@@ -21,6 +21,7 @@ import { UserAvatar } from "@/components/user-avatar";
 import { BotAvatar } from "@/components/bot-avatar";
 import { ReactMarkdown } from "react-markdown/lib/react-markdown";
 import { useProModal } from "@/hooks/useProModal";
+import { toast } from "react-hot-toast";
 
 const CodePage = () => {
   const router = useRouter();
@@ -51,6 +52,8 @@ const CodePage = () => {
     } catch (error: any) {
       if (error?.response?.status === 403) {
         proModal.onOpen();
+      } else {
+        toast.error("Something went wrong");
       }
     } finally {
       router.refresh();
--- app/(dashboard)/(routes)/video/page.tsx
+++ app/(dashboard)/(routes)/video/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import * as z from "zod";
-import Heading from "@/components/heading";
+import { Heading } from "@/components/heading";
 import { Video } from "lucide-react";
 import { useForm } from "react-hook-form";
 
@@ -16,6 +16,7 @@ import axios from "axios";
 import { Empty } from "@/components/empty";
 import { Loader } from "@/components/loader";
 import { useProModal } from "@/hooks/useProModal";
+import { toast } from "react-hot-toast";
 
 const VideoPage = () => {
   const router = useRouter();
@@ -40,6 +41,8 @@ const VideoPage = () => {
     } catch (error: any) {
       if (error?.response?.status === 403) {
         proModal.onOpen();
+      } else {
+        toast.error("Something went wrong");
       }
     } finally {
       router.refresh();
